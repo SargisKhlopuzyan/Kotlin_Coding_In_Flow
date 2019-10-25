9ae82b313366a0b8b2a0daf373a4a86864c955a4
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2019-10-25 09:31:03 +0400

    Hello, World!

---
+++ app/src/main/java/com/example/kotlin_coding_in_flow/KotlinTutorial.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_coding_in_flow
+
+/**
+ * Created by Joomag, Inc., on 10/25/2019.
+ *
+ * @author Sargis Khlopuzyan (s.khlopuzyan@joomag.com)
+ */
+
+fun main() {
+    println("Hello, World!")
+}
---
+++ app/src/main/java/com/example/kotlin_coding_in_flow/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_coding_in_flow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
